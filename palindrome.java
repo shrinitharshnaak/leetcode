b495dc3c3996526bf50ccbf30b9f47939797dc73
Shrinitharshnaa K <142794242+shrinitharshnaak@users.noreply.github.com>
2025-07-07 22:54:21 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class palindrome {
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+        sc.close();
+    }
+}
